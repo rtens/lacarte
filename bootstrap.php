22e16f351da33f0f7ea3ccf5ef5ec143ca10b28b
Nikolas Martens <Nikolas.M@rtens.org>
2013-04-14 01:49:46 +0200

    Made UserConfiguration optional

--- bootstrap.php
+++ bootstrap.php
@@ -1,13 +1,22 @@
 <?php
 
+use cfg\rtens\lacarte\DefaultConfiguration;
 use cfg\rtens\lacarte\UserConfiguration;
 use rtens\lacarte\core\Configuration;
 
 require __DIR__ . '/vendor/autoload.php';
-require_once __DIR__ . '/config/DefaultConfiguration.php';
-require_once __DIR__ . '/config/UserConfiguration.php';
 
 $factory = new \watoki\factory\Factory();
-$factory->setSingleton(Configuration::Configuration, new UserConfiguration());
+
+require_once __DIR__ . '/config/DefaultConfiguration.php';
+
+$userConfig = __DIR__ . '/config/UserConfiguration.php';
+if (file_exists($userConfig)) {
+    require_once $userConfig;
+    $factory->setSingleton(Configuration::Configuration, new UserConfiguration());
+} else {
+    $factory->setSingleton(Configuration::Configuration, new DefaultConfiguration());
+}
+
 
 return $factory;
