d4aa31f4c1ef136b7fcbcd5b22c1edd239db1dbf
Nikolas Martens <Nikolas.M@rtens.org>
2013-04-23 12:36:49 +0200

    Templates for #8, #9, #11, #12, #13, #14, #15

---
+++ spec/rtens/lacarte/features/order/CreateOrderTest.php
@@ -0,0 +1,16 @@
+<?php
+namespace spec\rtens\lacarte\features\order;
+
+use spec\rtens\lacarte\Test;
+
+class CreateOrderTest extends Test {
+
+    function testOneWeek() {
+        $this->markTestIncomplete();
+    }
+
+    function testOverTheWeekend() {
+        $this->markTestIncomplete();
+    }
+
+}
