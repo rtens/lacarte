8e0ef2658f6841728a713c18f5db144408dc4d45
Nikolas Martens <Nikolas.M@rtens.org>
2013-04-30 14:54:14 +0200

    Dont complain if build dir already exists

--- install.php
+++ install.php
@@ -1,7 +1,7 @@
 <?php
 
 echo "Downloading Composer installer..." . PHP_EOL;
-mkdir('build');
+@mkdir('build');
 file_put_contents("build/composer_installer.php", file_get_contents('http://getcomposer.org/installer'));
 
 echo "Installing composer.phar" . PHP_EOL;
