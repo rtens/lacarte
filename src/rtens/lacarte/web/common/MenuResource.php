23a377c8d35099fc6e1af2485a619efa19d511bf
Nikolas Martens <Nikolas.M@rtens.org>
2013-10-31 23:48:29 +0100

    Make src of menu image absolute

--- src/rtens/lacarte/web/common/MenuResource.php
+++ src/rtens/lacarte/web/common/MenuResource.php
@@ -3,6 +3,7 @@ namespace rtens\lacarte\web\common;
  
 use rtens\lacarte\Presenter;
 use watoki\curir\resource\DynamicResource;
+use watoki\dom\Element;
 
 class MenuResource extends DynamicResource {
 
@@ -13,7 +14,11 @@ class MenuResource extends DynamicResource {
 
     public function doGet() {
         return new Presenter(array(
-            'adminOnly' => $this->session->has('admin')
+            'adminOnly' => $this->session->has('admin'),
+            'relative' => function (Element $e) {
+                    $e->setAttribute('src', '/' . $this->getRoot()->getRoute()->toString() . '/common/' . $e->getAttribute('src')->getValue());
+                    return true;
+                }
         ));
     }
 
